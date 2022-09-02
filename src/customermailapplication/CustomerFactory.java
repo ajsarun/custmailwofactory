40b88a6ed58eb1ca52d1062c4675f8c8b783c74c
63050102 <63050102@kmitl.ac.th>
2022-09-02 09:28:15 +0700

    3.1(edit)

--- src/customermailapplication/CustomerFactory.java
+++ src/customermailapplication/CustomerFactory.java
@@ -4,30 +4,12 @@ public class CustomerFactory
 {
     public static Customer createCustomer(String type) 
     {
-        if (type.equalsIgnoreCase("Regular")){ return new RegularCustomer();}
+        if (type.equalsIgnoreCase("Regular")) { return new RegularCustomer();}
         else if (type.equalsIgnoreCase("Mountain")) { return new MountainCustomer();}
         else if (type.equalsIgnoreCase("Delinquent")) {return new DelinquentCustomer();}
 
         return null;
 
-    }
-
-    private Customer customer;
-    public void getCustomerTypeFromUser(String customerType) {
-        switch(customerType) {
-            case "Regular":
-                customer = new RegularCustomer();
-                break;
-            case "Mountain":
-                customer = new MountainCustomer();
-                break;
-            case "Delinquent":
-                customer = new DelinquentCustomer();
-                break;
-        }
-    }
-    public String generateMail() {
-        return customer.createMail();
     }
 }
 
